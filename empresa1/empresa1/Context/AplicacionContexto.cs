2dd8b1a187785ac4a7ab002503fd42b0eba7962a
mancogamer <tarquiroman@gmail.com>
2023-11-09 15:29:29 -0400

    subiendo modelo y controlador de Salario

--- empresa1/empresa1/Context/AplicacionContexto.cs
+++ empresa1/empresa1/Context/AplicacionContexto.cs
@@ -1,17 +1,16 @@
+using empresa1.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace empresa1.Context
+namespace Empresa1.Context
 {
     public class Aplication_Context : DbContext
     {
         public Aplication_Context(DbContextOptions<Aplication_Context> options)
             : base(options) { }
-        public DbSet<Departamento> departamento { get; set; }
 
-        public DbSet<Empelado> empleado { get; set; }
 
-        public DbSet<Titulo> titulo { get; set; }
-        
+        public DbSet<Salario> salario { get; set; }
+
 
     }
 }
---
+++ empresa1/empresa1/Controllers/SalarioController.cs
@@ -0,0 +1,75 @@
+﻿using empresa1.Models;
+using Empresa1.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace empresa1.Controllers
+{
+    [ApiController]
+
+    [Route("[controller]")]
+    public class SalarioController : ControllerBase
+    {
+
+        private readonly ILogger<SalarioController> _logger;
+
+        private readonly Empresa1.Context.Aplication_Context _aplication_context;
+        public SalarioController(
+            ILogger<SalarioController> logger,
+
+
+            Aplication_Context aplication_context)
+        {
+            _logger = logger;
+
+
+            _aplication_context = aplication_context;
+
+        }
+        /*Crear*/
+        [Route("")]
+        [HttpPost]
+        public IActionResult Post(
+            [FromBody] Salario salario)
+        {
+            _aplication_context.salario.Add(salario);
+
+            _aplication_context.SaveChanges();
+            return Ok(salario);
+
+        }
+        /*Obtener lista*/
+        [Route("")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_aplication_context.salario.ToList());
+
+        }
+        /*Modificar*/
+        [Route("{id}")]
+
+        [HttpPut]
+        public IActionResult Put(
+            [FromBody] Salario salario)
+        {
+            _aplication_context.salario.Update(salario);
+            _aplication_context.SaveChanges();
+
+            return Ok(salario);
+        }
+        /*Eliminar*/
+        [Route("{id}")]
+        [HttpDelete]
+        public IActionResult Delete(int id_salario)
+        {
+            _aplication_context.salario.Remove(
+                _aplication_context.salario.ToList()
+                .Where(x => x.idsalario == id_salario)
+
+                .FirstOrDefault());
+            _aplication_context.SaveChanges();
+
+            return Ok(id_salario);
+        }
+    }
+}
